c3b89efd298e3c5160cb6c74c43ea3f58ca313d1
Alex Hedley <alex.hedley@me.com>
2023-01-08 15:53:39 +0000

    Change layout

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
-var myApp = angular.module('myApp',[]);
-myApp.controller('myController', function ($scope, $http, $q, $filter) {
+var myApp = angular.module("myApp", []);
+myApp.controller("myController", function ($scope, $http, $q, $filter) {
+  // const pathPrefix = process.env.GITHUB_ACTIONS ? "/lego/" : "/";
+  const pathPrefix = "/lego/"; // "/"
+  $scope.pathPrefix = pathPrefix;
 
-    $scope.events = [];
+  $scope.events = [];
+  $scope.sum = 0;
 
-    $scope.init = function () {
-        getData();
-    }
+  $scope.init = function () {
+    getData();
+  };
 
-    getData = () =>  {
-        var file = 'data/sets.json';
+  getData = () => {
+    var file = "data/sets.json";
 
-        $http.get(file)
-        .then(function(response) {
-            $scope.sets = response.data.sets;
+    $http.get(file).then(function (response) {
+      $scope.sets = response.data.sets;
+
+      $scope.sum = function () {
+        var total = 0;
+        angular.forEach($scope.sets, function (key, value) {
+          total += parseFloat(key.price);
         });
-    };
+        return total;
+      };
+    });
+  };
 
-    $scope.init();
+  $scope.init();
 });
